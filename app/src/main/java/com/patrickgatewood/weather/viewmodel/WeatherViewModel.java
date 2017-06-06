a620335772e3f39e066d41b1a73154e66712991d
Patrick Gatewood <pg8wood@vt.edu>
2017-06-06 11:12:55 -0400

    Remove Java 8 support for Android compatibility issues

--- app/src/main/java/com/patrickgatewood/weather/viewmodel/WeatherViewModel.java
+++ app/src/main/java/com/patrickgatewood/weather/viewmodel/WeatherViewModel.java
@@ -2,10 +2,16 @@ package com.patrickgatewood.weather.viewmodel;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
-import android.zetterstrom.com.forecast.ForecastClient;
-import android.zetterstrom.com.forecast.models.Forecast;
+
+import com.patrickgatewood.weather.Constants;
+import com.patrickgatewood.weather.DarkSkyClient;
+import com.patrickgatewood.weather.data.WeatherApplication;
+import com.patrickgatewood.weather.model.Forecast;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,17 +26,27 @@ public class WeatherViewModel {
     }
 
     public void onFetchButtonTap(View view) {
-
-    }
-
-    private void fetchCurrentForecast() {
         Log.v("Weather", "API call initiated");
+        WeatherApplication weatherApplication = WeatherApplication.create(context);
+        DarkSkyClient darkSkyClient = weatherApplication.getDarkSkyClient();
 
-        // TODO get user's current location
-        double latitude = 38.029306;
-        double longitude = -78.476678;
+        // TODO get user's location
+        Call<List<Forecast>> apiCall = darkSkyClient.fetchCurrentForecast(
+                Constants.API_KEY, "38.029306", "-78.476678");
 
-        // TODO API call to current forecast
+        // Execute the call asynchronously. Get a positive or negative callback.
+        apiCall.enqueue(new Callback<List<Forecast>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<Forecast>> call, @Nullable Response<List<Forecast>> response) {
+                Log.v("Weather", "API call was a success!");
+            }
+
+            @Override
+            public void onFailure(Call<List<Forecast>> call, Throwable t) {
+                Log.v("Weather", "API call failed :(");
+            }
+        });
+
     }
 
 }
