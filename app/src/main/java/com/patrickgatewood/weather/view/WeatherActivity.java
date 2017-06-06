cfa1d4174dd3d253d74efe0a6fef93f857ef8af9
Patrick Gatewood <pg8wood@vt.edu>
2017-06-06 13:08:59 -0400

    Code cleanup

--- app/src/main/java/com/patrickgatewood/weather/view/WeatherActivity.java
+++ app/src/main/java/com/patrickgatewood/weather/view/WeatherActivity.java
@@ -1,7 +1,5 @@
 package com.patrickgatewood.weather.view;
 
-import android.icu.text.BreakIterator;
-import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
