623253408ae786824b987d8240f305cf8adb310e
Patrick Gatewood <pg8wood@vt.edu>
2018-02-10 20:46:02 -0500

    Pass activity lifecycle callbacks to react instance

--- android/app/src/main/java/com/patrickgatewood/weather/ui/WeatherActivity.java
+++ android/app/src/main/java/com/patrickgatewood/weather/ui/WeatherActivity.java
@@ -1,58 +1,48 @@
 package com.patrickgatewood.weather.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
-import android.provider.Settings;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.provider.Settings;
+import android.view.KeyEvent;
 import android.widget.Toast;
 
-import com.patrickgatewood.weather.R;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
+import com.patrickgatewood.weather.BuildConfig;
 
-import javax.inject.Inject;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-public class WeatherActivity extends AppCompatActivity implements WeatherView {
-
-    @BindView(R.id.fetchApiDataButton)
-    Button fetchApiDataButton;
-
-    @BindView(R.id.temperatureTextView)
-    TextView temperatureTextView;
-
-    @BindView(R.id.feelsLikeTextView)
-    TextView feelsLikeTextView;
-
-    @BindView(R.id.summaryTextView)
-    TextView summaryTextView;
-
-    @Inject
-    WeatherPresenter weatherPresenter;
+public class WeatherActivity extends Activity implements DefaultHardwareBackBtnHandler {
 
     public static int OVERLAY_PERMISSION_REQ_CODE = 1234;
+
+    private ReactRootView reactRootView;
+    private ReactInstanceManager reactInstanceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestOverlayPermission();
-        setContentView(R.layout.activity_weather_slider);
+        reactRootView = new ReactRootView(this);
+        reactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModulePath("index")
+                .addPackage(new MainReactPackage())
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+        reactRootView.startReactApplication(reactInstanceManager,
+                "MyReactNativeApp", null);
+
+        setContentView(reactRootView);
 
         ((WeatherApplication) getApplication()).getApplicationComponent().inject(this);
-        ButterKnife.bind(this);
-
-        weatherPresenter.attachView(this);
-        fetchApiDataButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                weatherPresenter.onFetchButtonClick();
-            }
-        });
     }
 
     /**
@@ -70,14 +60,30 @@ public class WeatherActivity extends AppCompatActivity implements WeatherView {
     }
 
     @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (reactInstanceManager != null) {
+            reactInstanceManager.onHostPause(this);
+        }
+    }
+
+    @Override
     protected void onResume() {
         super.onResume();
+
+        if (reactInstanceManager != null) {
+            reactInstanceManager.onHostResume(this, this);
+        }
     }
 
     @Override
     protected void onDestroy() {
-        weatherPresenter.detachView();
         super.onDestroy();
+
+        if (reactInstanceManager != null) {
+            reactInstanceManager.onHostDestroy(this);
+        }
     }
 
     @Override
@@ -93,12 +99,28 @@ public class WeatherActivity extends AppCompatActivity implements WeatherView {
     }
 
     @Override
-    public void updateCurrentConditionsTextViews(String temperature, String feelsLikeTemp, String summary) {
-        String temp = "Current temperature: " + temperature + " degrees";
-        String feelsLike = "Feels like: " + feelsLikeTemp + " degrees";
+    public void invokeDefaultOnBackPressed() {
+        super.onBackPressed();
+    }
 
-        temperatureTextView.setText(temp);
-        feelsLikeTextView.setText(feelsLike);
-        summaryTextView.setText(summary);
+    @Override
+    public void onBackPressed() {
+        if (reactInstanceManager != null) {
+            reactInstanceManager.onBackPressed();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    /**
+     * Override the hardware menu button to show the react native dev menu
+     */
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU && reactInstanceManager != null) {
+            reactInstanceManager.showDevOptionsDialog();
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
     }
 }
